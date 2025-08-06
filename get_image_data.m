fe8b2a904c8b80d751e19b9b44d23bc2a117e0e1
sheffiha <sheffiha@rose-hulman.edu>
2025-08-05 21:15:28 -0400

    getting the data with only the code as the label

---
+++ get_image_data.m
@@ -0,0 +1,39 @@
+rootdir = 'C:\Users\sheffiha\Documents\Junior-Summer\ImageRecognition\CSSE463_Final_Project-Leaf-\archive (4)\Plants_2\';
+subdir = [rootdir 'train'];
+
+trainImages = imageDatastore(...
+    subdir, ...
+    'IncludeSubfolders',true, ...
+    'LabelSource', 'foldernames');
+
+% Make datastores for the validation and testing sets similarly.
+subdir = [rootdir 'test'];
+testImages = imageDatastore(...
+    subdir, ...
+    'IncludeSubfolders',true, ...
+    'LabelSource', 'foldernames');
+
+subdir = [rootdir 'validate'];
+validateImages = imageDatastore(...
+    subdir, ...
+    'IncludeSubfolders',true, ...
+    'LabelSource', 'foldernames');
+
+% REPLACE WITH THE NET THAT YOU ARE USING
+net = darknet53;
+inputSize = net.Layers(1).InputSize;
+
+
+augimdsTrain = augmentedImageDatastore(inputSize(1:2),trainImages);
+augimdsTest = augmentedImageDatastore(inputSize(1:2),testImages);
+augimdsValidate = augmentedImageDatastore(inputSize(1:2),validateImages);
+
+% Anonymous functions for matlab would be like a lambda in python
+extractCode = @(s) regexp(s, '\((P\d+)', 'tokens', 'once');
+
+% can apply the function to the whole cell array with cell fun
+% need to use cell str to make the categorical it cell array
+% then need to change back to categorical
+yTrain = categorical(cellfun(@(s) extractCode(s), cellstr(trainImages.Labels)));
+yTest = categorical(cellfun(@(s) extractCode(s), cellstr(testImages.Labels)));
+yValidate = categorical(cellfun(@(s) extractCode(s), cellstr(validateImages.Labels)));
